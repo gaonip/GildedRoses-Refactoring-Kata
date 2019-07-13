e0d019f22c89efc5d857045a8aee91c04388249f
kai zhang <kaizhi.zhang92@gmail.com>
2019-07-13 14:56:02 +0200

    Create a new refactored updateQuality2 method

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -56,6 +56,46 @@ class GildedRose(val items: Array[Item]) {
     }
   }
 
+  // Function to make sure a given threshold value is never exceeded
+  def set_max_quality(item: Item, updateValue:Int, max_quality : Int) : Item = {
+
+    item.quality += updateValue
+    item.quality = item.quality min max_quality
+    item
+  }
+
+  // Refactored original function to update quality
+  def updateQuality2(): Unit ={
+    items.foreach {
+      case item@Item("Sulfuras, Hand of Ragnaros", _, _) => //do nothing
+      case item@Item("Aged Brie", sellIn, quality) if quality <= 50 & sellIn >= 0 =>
+        item.sellIn -= 1
+        //item.quality += 1
+        set_max_quality(item, 1, 50)
+      case item@Item("Aged Brie", sellIn, quality) if quality <= 50 & sellIn < 0 =>
+        item.sellIn -= 1
+        set_max_quality(item, 2, 50)
+      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) if quality < 50 && sellIn > 10 =>
+        item.sellIn -= 1
+        set_max_quality(item, 1, 50)
+      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) if quality < 50 && sellIn > 5 =>
+        item.sellIn -= 1
+        set_max_quality(item, 2, 50)
+      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality) if quality < 50 && sellIn > 0 =>
+        item.sellIn -= 1
+        set_max_quality(item, 3, 50)
+      case item@Item("Backstage passes to a TAFKAL80ETC concert", sellIn, _) if sellIn <= 0 =>
+        item.sellIn -= 1
+        item.quality = 0
+      case item@Item(_, sellIn, quality) if sellIn > 0 && quality > 0 =>
+        item.sellIn -= 1
+        item.quality -= 1
+      case item@Item(_, sellIn, quality) if sellIn <= 0 && quality > 0 =>
+        item.sellIn -= 1
+        item.quality -= 2
+      case _ =>
+    }
+  }
 
 }
 
--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -22,69 +22,69 @@ class GildedRoseTest extends FunSpec with BeforeAndAfterEach {
   describe("GildedRose") {
 
     it("should degrade items in quality by one") {
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(regular.quality == 4)
     }
 
     it("should reduce items in sellIn by one") {
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(regular.sellIn == 4)
     }
 
     it("should degrade twice as fast once sellIn has passed"){
       regular.sellIn = 0
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(regular.quality == 3)
     }
 
     it("should not have a quality lower than zero") {
       regular.quality = 0
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(regular.quality == 0)
     }
 
     it("should make sure aged brie increases in quality the older it gets"){
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(agedBrie.quality == 11)
     }
 
     it("should make sure aged brie increases in quality again (+1 +1) as SellIn value <0"){
       agedBrie.sellIn = -1
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(agedBrie.quality == 12)
     }
 
     it("should make sure quality of an item never is more than 50"){
       agedBrie.quality = 50
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(agedBrie.quality == 50)
     }
 
     it("should make sure sulfuras never degrades in quality or be sold"){
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(sulfuras.quality == 80 && sulfuras.sellIn == 20)
     }
 
     it("should make sure backstage pass quality increases when the sellIn value > 10"){
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(backstage.quality == 9)
     }
 
     it("should make sure backstage pass quality increases by 2 when the sellIn value > 5"){
       backstage.sellIn = 10
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(backstage.quality == 10)
     }
 
     it("should make sure backstage pass quality increases by 3 when the sellIn value > 0"){
       backstage.sellIn = 5
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(backstage.quality == 11)
     }
 
     it("should make sure backstage pass quality drops to 0 when the sellIn value reaches 0"){
       backstage.sellIn = 0
-      rose.updateQuality()
+      rose.updateQuality2()
       assert(backstage.quality == 0)
     }
 
